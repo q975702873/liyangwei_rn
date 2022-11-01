1798bbe6cad62febc90217a4383006f5e3d63e75
yunxi <yunxi@yunxideMacBook-Pro-2.local>
2022-11-01 14:50:59 +0800

    修复android包找不到

--- android/src/main/java/com/liyangwei/demo/RNRnDemoModule.java
+++ android/src/main/java/com/liyangwei/demo/RNRnDemoModule.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.widget.Toast;
 
 public class RNRnDemoModule extends ReactContextBaseJavaModule {
 
